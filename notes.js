674fe536eceacf4b5a2179b5f7a33135a579ea76
PrinceCam <cjones.prsvr@gmail.com>
2024-04-25 20:19:23 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,64 @@
+// let myPoints = 3
+// // Create two functions, add3Points() and removePoints(), and have them
+// // add/remove points to/from the myPoints variable
+// function add3Points() {
+//  myPoints += 3
+// }
+// function remove1Points() {
+//     myPoints -= 1
+// }
+// add3Points
+// add3Points
+// add3Points
+// remove1Points()
+// remove1Points()
+// // Call the functions to that the line below logs out at 10
+// console.log(myPoints)
+// Try to predict what each of the lines will log out
+// console.log("2" +2)  (22 answer)
+// console.log(11 + 7)  (18 answer)
+// console.log(6 + "5")  (65 answer)
+// console.log("My Points: " + 5 + 9)  (My Points: 59)
+// console.log(2 + 2)  (4 answer)
+// console.log("11" + "14")  (1114 answer)
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+// let paragraph = document.getElementById("error")
+// console.log(errorParagraph)
+// function purchase() {
+//     console.log("button clicked")
+//     errorParagraph.textContext = "Something went wrong, please"
+// }
+let num1 = 8
+let num2 = 2
+document.getElementId("num1-el").textContent = num1
+document.getElementId("num2-el").textContent = num2
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculate using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+// let num1 = 8
+// let num2 = 2
+// document.getElementId("num1-el").textContent = num1
+// document.getElementId("num2-el").textContent = num2
+// let sumEl = document.getElementId("sum-el")
+
+function add() {
+console.log("add clicked")
+}
+function subtract() {
+}
+function divide() {
+}
+function multiply() {
+}
+
+
+
+
+
+
+
